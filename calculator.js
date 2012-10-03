a547500083349eca41809c64559a11437ee0ebd6
Douglas Rodrigo Ferreira <douglasrodrigo@gmail.com>
2012-10-03 19:26:29 -0300

    implementacao de uma calculadora em html

---
+++ calculator.js
@@ -0,0 +1,16 @@
+$("#calcular").click(function() {
+    var resultado;
+
+    var numA = parseInt($("#num-a").val());
+    var numB = parseInt($("#num-b").val());
+
+    if ($("#operator").val() == "+") {
+        resultado = numA + numB;
+    }
+
+    if ($("#operator").val() == "*") {
+        resultado = numA * numB;
+    }
+    $("#resultado").html(resultado);
+
+});
