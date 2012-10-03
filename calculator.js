78e55d291c692c30f90f08ee59bfbf49ce6951a0
Rafael Nicoletti <rafael@ti24horas.com.br>
2012-10-03 20:56:06 -0300

    adicionando validacao para numero a e b

--- calculator.js
+++ calculator.js
@@ -1,6 +1,14 @@
 $("#calcular").click(function() {
     var resultado;
-
+    regvalidation = /^[\d]+$/;
+    if (!regvalidation.test($("#num-a").text())){
+        alert('numero a invalido');
+	   return false;
+    }
+    if (!regvalidation.test($("#num-b").text())){
+	   alert ('numero b invalido');
+	   return false;
+    }
     var numA = parseInt($("#num-a").val());
     var numB = parseInt($("#num-b").val());
 
