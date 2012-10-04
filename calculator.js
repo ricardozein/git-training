f9d58cff2b7440f7739a06ab01fadbd68559837f
Douglas Rodrigo Ferreira <douglasrodrigo@gmail.com>
2012-10-03 21:24:54 -0300

    mudando a implementacao da calculadora para utilizar o eval

--- calculator.js
+++ calculator.js
@@ -1,16 +1,3 @@
 $("#calcular").click(function() {
-    var resultado;
-
-    var numA = parseInt($("#num-a").val());
-    var numB = parseInt($("#num-b").val());
-
-    if ($("#operator").val() == "+") {
-        resultado = numA + numB;
-    }
-
-    if ($("#operator").val() == "*") {
-        resultado = numA * numB;
-    }
-    $("#resultado").html(resultado);
-
+    $("#resultado").html(eval($("#num-a").val() + $("#operator").val() + $("#num-b").val()));
 });
